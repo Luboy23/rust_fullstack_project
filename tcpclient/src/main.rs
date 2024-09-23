6e9ca56111d17ed1fbb45b188006d8a2b9cc8f74
Luboy23 <2387443867@qq.com>
2024-09-23 14:27:56 +0800

    features: tcpserver and tcpistener

---
+++ tcpclient/src/main.rs
@@ -0,0 +1,13 @@
+use core::str;
+use std::net::TcpStream;
+use std::io::{Read, Write};
+
+fn main() {
+    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+
+    let mut buffer = [0; 5];
+    stream.read(&mut buffer).unwrap();
+    println!("Response from server: {:?}",
+    str::from_utf8(&buffer).unwrap());
+}
